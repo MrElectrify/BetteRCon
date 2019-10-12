70502247b15a91d65402d10ec598c4ccdba1f0b8
Andrew Buck <aman560900@hotmail.com>
2019-10-11 23:59:31 -0500

    CMake instead of bash for better cross-platform

--- BetteRCon/BetteRConFramework/build.sh
+++
@@ -1,2 +0,0 @@
-g++ -c --std=c++17 src/*.cpp src/Internal/*.cpp -Iinclude -I../../asio/asio/include
-ar rvs libBetteRConFramework.lib *.o
--- BetteRCon/BetteRConTestBench/build.sh
+++
@@ -1,1 +0,0 @@
-g++ BetteRConTestBench.cpp -I ../BetteRConFramework/include -I ../../asio/asio/include/ --std=c++17 ../BetteRConFramework/libBetteRConFramework.a -lpthread -oBetteRConTestBench
