06e32adce98c31fa6ec9614b593178af786e7b89
Andrew Buck <aman560900@hotmail.com>
2019-12-09 00:54:50 -0600

    Pesky linux EOLs, make folders if they do not exist

--- linux/buildBRF.sh
+++ linux/buildBRF.sh
@@ -1,3 +1,4 @@
+[ ! -d "lib/" ] && mkdir lib
 g++ --std=c++17 -fPIC -Wall -I../include -I../dependencies/asio/asio/include -I../dependencies/MD5 ../src/Internal/Connection.cpp ../src/Internal/ErrorCode.cpp ../src/Internal/Packet.cpp ../src/Server.cpp ../dependencies/MD5/MD5.cpp -c
 ar rcs libBetteRConFramework.a Connection.o ErrorCode.o Packet.o Server.o MD5.o
 mv libBetteRConFramework.a lib/
--- linux/buildFRS.sh
+++ linux/buildFRS.sh
@@ -1,1 +1,2 @@
+[ ! -d "plugins/" ] && mkdir plugins
 g++ --std=c++17 -fPIC -I../include -I../dependencies/asio/asio/include -Llib ../src/FastRoundStart.cpp -Wl,-Bstatic -lBetteRConFramework -Wl,-Bdynamic -lpthread -ldl -lstdc++fs -shared -oplugins/FastRoundStart.plugin
