97d39c37d858002df52388f27c32d37aa441b616
Andrew Buck <aman560900@hotmail.com>
2019-12-18 00:10:55 -0500

    Move function scopes

--- src/Assist.cpp
+++ src/Assist.cpp
@@ -133,6 +133,8 @@ public:
 		});
 	}
 
+	virtual ~Assist() {}
+private:
 	void ReadPlayerDatabase()
 	{
 		// try to open the database
@@ -293,11 +295,11 @@ public:
 			if (teamSize >= maxTeamSize)
 				// there is not enough space. wait until the next time around
 				break;
-			
+
 			// we are good to switch them. let's do it
 			MovePlayer(newTeamId, UINT8_MAX, pPlayer);
 
-			SendChatMessage("Thanks for assisting the losing team, "+ pPlayer->name + "!\n", pPlayer);
+			SendChatMessage("Thanks for assisting the losing team, " + pPlayer->name + "!\n", pPlayer);
 
 			m_moveQueue.pop_front();
 		}
@@ -667,7 +669,7 @@ public:
 		const std::string& playerName = eventArgs[1];
 
 		const MoveQueue_t::iterator playerQueueIt = std::find(m_moveQueue.begin(), m_moveQueue.end(), playerName);
-		
+
 		// they are not in the move queue, ignore them
 		if (playerQueueIt == m_moveQueue.end())
 			return;
@@ -687,8 +689,6 @@ public:
 		SendChatMessage("You have been removed from the queue because you manually switched. Thanks for helping the losing team!", playerIt->second);
 	}
 
-	virtual ~Assist() {}
-private:
 	// scores
 	std::vector<int32_t> m_lastScores;
 	std::vector<int32_t> m_lastScoreDiffs;
