ac7fce851630ce7c7e2987222c53475a11da1b17
Andrew Buck <aman560900@hotmail.com>
2019-12-21 19:21:07 -0500

    Modify PendingVIPMap_t, implement WrtitePendingVIPDatabase

--- src/VIPManager.cpp
+++ src/VIPManager.cpp
@@ -29,11 +29,42 @@ private:
 
 	using PlayerInfo_t = BetteRCon::Server::PlayerInfo;
 	using PlayerMap_t = BetteRCon::Server::PlayerMap_t;
-	using PendingVIPMap_t = std::unordered_map<std::string, std::chrono::system_clock::duration>;
+	using PendingVIPMap_t = std::unordered_map<std::string, std::string>;
 	using VIPMap_t = std::unordered_map<std::string, VIP>;
 
 	PendingVIPMap_t m_pendingVIPs;
 	VIPMap_t m_VIPs;
+
+	std::chrono::system_clock::duration ParseDuration(const std::string& durationStr)
+	{
+		// parse the duration
+		const char durationType = durationStr.back();
+		int32_t durationCoefficient;
+		try
+		{
+			durationCoefficient = std::stoi(durationStr.substr(0, durationStr.size() - 1));
+		}
+		catch (const std::exception&)
+		{
+			BetteRCon::Internal::g_stdErrLog << "[VIPManager] Failed to parse duration coefficient: " << durationStr.substr(0, durationStr.size() - 1) << '\n';
+			return;
+		}
+
+		switch (durationType)
+		{
+		case 'h':
+			return Hours_t(durationCoefficient);
+		case 'd':
+			return Days_t(durationCoefficient);
+		case 'w':
+			return Weeks_t(durationCoefficient);
+		case 'm':
+			return Months_t(durationCoefficient);
+		default:
+			BetteRCon::Internal::g_stdErrLog << "[VIPManager] Failed to parse duration type: " << durationType << '\n';
+			return;
+		}
+	}
 
 	void ReadPendingVIPDatabase()
 	{
@@ -44,92 +75,50 @@ private:
 
 		// read each VIP with their duration
 		std::string pendingVIPLine;
-		size_t line = 0;
 		while (inFile >> pendingVIPLine)
 		{
 			const size_t comma = pendingVIPLine.find(',');
 			if (comma == std::string::npos)
 			{
-				BetteRCon::Internal::g_stdErrLog << "[VIPManager] Failed to find comma for pending VIP on line " << line << '\n';
+				BetteRCon::Internal::g_stdErrLog << "[VIPManager] Failed to find comma for pending VIP\n";
 				inFile.close();
 				Disable();
 				return;
 			}
 
 			// split the name and duration up, and add it to our admin maps
-			std::string VIPName = pendingVIPLine.substr(0, comma);
-			std::string VIPDurationStr = pendingVIPLine.substr(comma + 1);
-
-			std::chrono::system_clock::duration VIPDuration;
-
-			// parse the duration
-			const char durationType = VIPDurationStr.back();
-			int32_t durationCoefficient;
-			try
-			{
-				durationCoefficient = std::stoi(VIPDurationStr.substr(0, VIPDurationStr.size() - 1));
-			}
-			catch (const std::exception&)
-			{
-				BetteRCon::Internal::g_stdErrLog << "[VIPManager] Failed to parse duration coefficient for " << VIPName << " on line " << line << '\n';
-				inFile.close();
-				Disable();
-				return;
-			}
-
-			switch (durationType)
-			{
-			case 'h':
-				VIPDuration = Hours_t(durationCoefficient);
-				break;
-			case 'd':
-				VIPDuration = Days_t(durationCoefficient);
-				break;
-			case 'w':
-				VIPDuration = Weeks_t(durationCoefficient);
-				break;
-			case 'm':
-				VIPDuration = Months_t(durationCoefficient);
-				break;
-			default:
-				BetteRCon::Internal::g_stdErrLog << "[VIPManager] Failed to parse duration type for " << VIPName << " on line " << line << '\n';
-				inFile.close();
-				Disable();
-				return;
-			}
+			std::string name = pendingVIPLine.substr(0, comma);
+			std::string duration = pendingVIPLine.substr(comma + 1);
 
 			// see if they are already a VIP
-			const VIPMap_t::iterator vipIt = m_VIPs.find(VIPName);
+			const VIPMap_t::iterator vipIt = m_VIPs.find(name);
 			if (vipIt != m_VIPs.end())
 			{
 				// we are updating an existing VIP. add the time
-				vipIt->second.expiry += VIPDuration;
+				vipIt->second.expiry += ParseDuration(duration);
 
 				// check to make sure they have not expired
 				if (std::chrono::system_clock::now() > vipIt->second.expiry)
 					m_VIPs.erase(vipIt);
 
-				++line;
 				continue;
 			}
 
 			// check to see if the player is in-game already
 			const PlayerMap_t players = GetPlayers();
 
-			const PlayerMap_t::const_iterator playerIt = players.find(VIPName);
+			const PlayerMap_t::const_iterator playerIt = players.find(name);
 			if (playerIt != players.end())
 			{
 				// the player is in-game. give them VIP status
 				const std::shared_ptr<PlayerInfo_t> pPlayer = playerIt->second;
-				m_VIPs.emplace(std::move(VIPName), VIP{ pPlayer->name, pPlayer->GUID, std::chrono::system_clock::now() + VIPDuration });
+				m_VIPs.emplace(std::move(name), VIP{ pPlayer->name, pPlayer->GUID, std::chrono::system_clock::now() + ParseDuration(duration) });
 
-				++line;
 				continue;
 			}
 
 			// create a new pending VIP with the duration
-			m_pendingVIPs.emplace(std::move(VIPName), VIPDuration);
-			++line;
+			m_pendingVIPs.emplace(std::move(name), duration);
 		}
 
 		inFile.close();
@@ -139,7 +128,16 @@ private:
 	}
 	void WritePendingVIPDatabase() 
 	{
+		// open the output file
+		std::ofstream outFile("plugins/PendingVIPs.cfg");
+		if (outFile.good() == false)
+			return;
 
+		// write each admin
+		for (const PendingVIPMap_t::value_type& pendingVIP : m_pendingVIPs)
+			outFile << pendingVIP.first << ',' << pendingVIP.second << '\n';
+
+		outFile.close();
 	}
 
 	void ReadVIPDatabase() {}
